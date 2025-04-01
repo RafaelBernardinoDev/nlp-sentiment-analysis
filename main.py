2da742f13c9013675dd7cd10d279624ee69f6196
RafaelBernardinoDev <rafaelbernardinoriveira2@gmail.com>
2025-04-01 17:04:11 -0300

    add grafico de frequencia

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
 from sklearn.linear_model import LogisticRegression
+import nltk
+from nltk import tokenize
 import pandas as pd
+import matplotlib.pyplot as plt
+import seaborn as sns
 
 df = pd.read_csv('data/raw.csv')
 
@@ -21,4 +25,29 @@ def classficar_texto(texto, coluna_texto, coluna_classificacao):
     return print(f"Acurácia do modelo com : {acuracia * 100:.2f}%")
 
 
+def grafico_frequencia(texto, coluna_texto, quantidade):  
+    # Juntando todos os textos em uma única string
+    todas_palavras = ' '.join([texto for texto in texto[coluna_texto]])
+    
+    # Calculando a frequência das palavras
+    token_espaco = tokenize.WhitespaceTokenizer()
+    frequencia = nltk.FreqDist(token_espaco.tokenize(todas_palavras))
+    
+    # Criando um DataFrame com as palavras e suas frequências
+    df_frequencia = pd.DataFrame({"Palavra": list(frequencia.keys()),
+                                  "Frequência": list(frequencia.values())})
+    
+    # Selecionando as palavras mais frequentes
+    df_frequencia = df_frequencia.nlargest(columns="Frequência", n=quantidade)
+    
+    # Criando o gráfico de barras
+    plt.figure(figsize=(20,6))
+    ax = sns.barplot(data=df_frequencia, x="Palavra", y="Frequência", color='gray')
+    ax.set(ylabel="Contagem")
+    
+    # Exibindo o gráfico
+    plt.show()
+
+
 classficar_texto(df, df.avaliacao, df.sentimento)
+grafico_frequencia(df, df.avaliacao, df.sentimento)
